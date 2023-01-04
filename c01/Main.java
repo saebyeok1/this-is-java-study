3167220e9023fa783eb0e47c97fc00cf49192646
saebyeok1 <qhdid3734@gmail.com>
2023-01-04 21:56:22 +0900

    feat: hi

--- c01/Main.java
+++ c01/Main.java
@@ -2,6 +2,8 @@ package c01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello ch1");
+        System.out.println("hello hello");
+        System.out.println("hi ch1");
+        System.out.println("hi ch1");
     }
 }
