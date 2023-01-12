61985966565e589970e387daae479b6d5b29bcaf
saebyeok1 <qhdid3734@gmail.com>
2023-01-13 05:24:54 +0900

    feat: ch06 question (Day4)

---
+++ ch06/test/num13.java
@@ -0,0 +1,13 @@
+package ch06.test;
+
+public class num13 {
+    String name;
+    String id;
+    String password;
+    int age;
+
+    num13(String name,String id){
+        this.name = name;
+        this.id = id;
+    }
+}
